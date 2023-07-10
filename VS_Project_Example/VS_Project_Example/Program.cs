a3e6115e196d5507344ec9de81114fd30e0c9779
DJ <epicmrjones@gmail.com>
2023-07-10 09:49:37 -0500

    init

---
+++ VS_Project_Example/VS_Project_Example/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace VS_Project_Example
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World! from a new repo");
+        }
+    }
+}
